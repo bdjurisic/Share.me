7aa70cdde423ffcc2f3bab601504b4033cf110d0
bdjurisic <d.j.balsa93@gmail.com>
2016-05-04 00:47:24 +0200

    *Create event

--- app/src/main/java/com/example/balsa/sharemev10/Create_Activity_Step_final.java
+++ app/src/main/java/com/example/balsa/sharemev10/Create_Activity_Step_final.java
@@ -1,7 +1,6 @@
 package com.example.balsa.sharemev10;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
